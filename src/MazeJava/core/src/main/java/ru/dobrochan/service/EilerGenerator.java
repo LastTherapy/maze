74fe163e116882550c1f251c43601a757295a5bd
helllomind <helllomind@hellomindmac.local>
2024-12-06 16:28:07 +0300

    eiler generagor correction

--- src/MazeJava/core/src/main/java/ru/dobrochan/service/EilerGenerator.java
+++ src/MazeJava/core/src/main/java/ru/dobrochan/service/EilerGenerator.java
@@ -14,7 +14,6 @@ public class EilerGenerator implements MazeGenerator {
     @Override
     public Maze generateMaze(int width, int height) {
         Maze maze = new Maze();
-
         Map<Integer, Set<Integer>> mazeSet = new HashMap<>();
         int lastSet = 1;
 
@@ -29,7 +28,6 @@ public class EilerGenerator implements MazeGenerator {
             line[i] = lastSet;
             lastSet++;
         }
-
         for (int i = 0; i < height; i++) {
             // 3. решаем ставить стенку српава или нет
             for (int j = 0; j < width - 1; j++) {
@@ -55,39 +53,34 @@ public class EilerGenerator implements MazeGenerator {
                     mazeSet.get(line[j]).addAll(rightSet);
                     mazeSet.remove(oldSet);
                 }
-
             }
-            // 4. решаем ставить стенку снизу или нет
-            for (int j = 0; j < width; j++) {
+            // 4. решаем ставить стенку снизу или нет. Не имеет смысла для последней строчки.
+            if (i < height - 1) {
+                for (int j = 0; j < width; j++) {
 //            4.2 Стенку решили ставить, стенку будем ставить, если множество,
 //            которому принадлежит текущая ячейка имеет более одной ячейки без нижней границы
-                if (getWallDecision()) {
-                    Set<Integer> set = mazeSet.get(line[j]);
-                    if (set.size() > 1) {
-                        matrixB[i][j] = 1;
+                    if (getWallDecision()) {
+                        Set<Integer> set = mazeSet.get(line[j]);
+                        if (set.size() > 1) {
+                            matrixB[i][j] = 1;
+                            // сразу же подготавливаем строку для следующего шага
+                            // создаем новое множество там где были ниние ячейки
+                            set.remove(j);
+                            line[j] = lastSet;
+                            mazeSet.computeIfAbsent(lastSet, k -> new HashSet<>()).add(j);
+                            lastSet++;
+                        }
                     }
-                    set.remove(j);
-                    // сразу же подготавливаем строку для следующего шага
-                    // создаем новое множество там где были ниние ячейки
-                    line[j] = lastSet;
-                    mazeSet.computeIfAbsent(lastSet, k -> new HashSet<>()).add(j);
-                    lastSet++;
                 }
             }
-
             if (i == height - 1) {
-                System.out.println("DEBUG MODE");
-
                 for (int j = 0; j < width; j++) {
-                    System.out.print(" " + line[j]);
-
 // 5.2 Если решили что, текущая строка последняя, то каждой ячейке присвойте нижнюю стенку и:
                     matrixB[i][j] = 1;
 //                Двигаясь слева направо, если множество текущей клетки
 //                и клетки справа не совпадает уберите между ними стенку (для того чтобы убрать замкнутые участки).
 //                Объедините множества текущей ячейки и ячейки справа.
                     if (j < width - 1 && line[j] != line[j + 1]) {
-
                         System.out.println("\n" + line[j] + " left " + line[j + 1] + "right - removing right wall");
                         matrixA[i][j] = 0;
                         int oldSet = line[j + 1];
@@ -99,7 +92,6 @@ public class EilerGenerator implements MazeGenerator {
                         }
                         mazeSet.get(line[j]).addAll(rightSet);
                         mazeSet.remove(oldSet);
-
                     }
                 }
                 System.out.println();
@@ -114,7 +106,6 @@ public class EilerGenerator implements MazeGenerator {
                         lastSet++;
                     }
                 }
-
             }
         }
         maze.setWidth(width);
@@ -124,9 +115,6 @@ public class EilerGenerator implements MazeGenerator {
         return maze;
     }
 
-    private void concatSets(int leftSet, int rightSet) {
-
-    }
 
     private boolean getWallDecision() {
         return Math.random() > 0.5;
